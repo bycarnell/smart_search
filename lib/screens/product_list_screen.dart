3bad4365bd214cb005cce70699b782823c20f630
carnell0 <carnellthon0@gmail.com>
2026-09-06 15:43:35 +0100

    Refactor ProductListScreen to improve code readability and structure

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -5,37 +5,45 @@ import 'package:smart_search/providers/product_list_provider.dart';
 import 'package:smart_search/providers/search_query_provider.dart';
 
 class ProductListScreen extends ConsumerWidget {
-
   const ProductListScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final products = ref.watch(filteredProductsProvider);
-    
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Liste des produits'),
-      ),
+      appBar: AppBar(title: const Text('Liste des produits')),
       body: Column(
         children: [
           TextField(
-            onChanged: (value) => ref.read(searchQueryProvider.notifier).state = value,
+            onChanged: (value) =>
+                ref.read(searchQueryProvider.notifier).state = value,
           ),
-          Text('${products.length} résultat(s)'),
+          Consumer(
+            builder: (context, ref, child) {
+              final products = ref.watch(filteredProductsProvider);
+              return Text('${products.length} résultat(s)');
+            },
+          ),
           Expanded(
-            child: ListView.builder (
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                final product = products[index];
-                return ListTile(
-                  title: Text(product.name),
-                  subtitle: Text(product.category),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      ref.read(productListProvider.notifier).deleteProduct(product.id);
-                    },
-                  ),
+            child: Consumer(
+              builder: (context, ref, child) {
+                final products = ref.watch(filteredProductsProvider);
+                return ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    final product = products[index];
+                    return ListTile(
+                      title: Text(product.name),
+                      subtitle: Text(product.category),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          ref
+                              .read(productListProvider.notifier)
+                              .deleteProduct(product.id);
+                        },
+                      ),
+                    );
+                  },
                 );
               },
             ),
